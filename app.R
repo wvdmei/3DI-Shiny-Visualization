560207273f7ce567ca88e49ce68fcf322c6b0501
Willem van der Mei <willem.vandermei91@gmail.com>
2021-11-29 19:30:43 -0500

    Most issues have been fixed. Need to polish heatmap and fix error upon loading.

--- app.R
+++ app.R
@@ -7,27 +7,61 @@
 #    http://shiny.rstudio.com/
 #
 
+# Load the required packages
 library(shiny)
 library(ggplot2)
 library(readr)
 library(plotly)
 
-createPlot <- function(outcome, plotType, objectDrug, basePrecipitant, estimateRange){
-    if(plotType == "volcano"){
-        basePlot <- ggplot(data = dplyr::filter(estimates, Object == objectDrug & Base_Precipitant == basePrecipitant & range == estimateRange & Outcome == outcome), aes(x = log2RR, y = log10P, label = paste0(SB_RR, ", 95% CI = (", SB_RR_lower, ", ", SB_RR_upper,"), p = ", pSB))) + 
-            geom_point(color = "red") + theme_bw() + scale_y_continuous(name = "log10(1/p)") + scale_x_continuous(name = "log2(semi-Bayes shrunk adjusted rate ratio)")
-        ggplotly(basePlot)
-        
-    }
-    else if(plotType == "heatMap"){
-        
-    }
+# Create a function for plot creation
+createVolcanoPlot <- function(outcome, objectDrug, basePrecipitant, estimateRange){
+        if(outcome == "hipfracture"){
+            outcomeFormatted <- "Hip Fracture"
+        }
+        else if(outcome == "mvc"){
+            outcomeFormatted <- "Motor Vehicle Collision"
+        }
+        else {
+            outcomeFormatted <- "Injury"
+        }
+        titleText <- paste0("Volcano Plot of Adjusted RRs of ", stringr::str_to_title(outcomeFormatted), " for ", stringr::str_to_sentence(objectDrug), ", ", stringr::str_to_sentence(basePrecipitant), ", and Precipitants")
+        filteredData <- dplyr::filter(estimates, Object == objectDrug & Base_Precipitant == basePrecipitant & range == estimateRange & Outcome == outcome)
+        `Precipitant Drug` <-  paste0(stringr::str_to_sentence(filteredData$Precipitant), ", Semi-Bayes Risk Ratio = ", round(filteredData$SB_RR, 2), ", 95% CI = (", round(filteredData$SB_RR_lower, 2), ", ", round(filteredData$SB_RR_upper,2),"), p = ", round(filteredData$pSB, 3))
+        basePlot <- ggplot(data = filteredData, aes(x = log2RR, y = log10P, label = `Precipitant Drug`)) + 
+            geom_point(color = "red") + theme_bw() + scale_y_continuous(name = "log10(1/p)") + scale_x_continuous(name = "log2(semi-Bayes shrunk adjusted rate ratio)") + ggtitle(titleText)
+        ggplotly(basePlot, tooltip = "label")
     
 }
 
+createHeatMapPlot <- function(outcome, objectDrug, estimateRange){
+    if(outcome == "hipfracture"){
+        outcomeFormatted <- "Hip Fracture"
+    }
+    else if(outcome == "mvc"){
+        outcomeFormatted <- "Motor Vehicle Collision"
+    }
+    else {
+        outcomeFormatted <- "Injury"
+    }
+        titleText <- paste0("Heatmap of Adjusted RRs of ", stringr::str_to_title(outcomeFormatted), " for ", stringr::str_to_sentence(objectDrug), ", Base Precipitants, and Precipitants")
+        filteredData <- dplyr::filter(estimates, Object == objectDrug & range == estimateRange & Outcome == outcome)
+        #`Precipitant Drug` <-  paste0(stringr::str_to_sentence(filteredData$Precipitant), ", Semi-Bayes Risk Ratio = ", round(filteredData$SB_RR, 2), ", 95% CI = (", round(filteredData$SB_RR_lower, 2), ", ", round(filteredData$SB_RR_upper,2),"), p = ", round(filteredData$pSB, 3))
+        #basePlot <- ggplot(data = filteredData, aes(x = Precipitant, y = Base_Precipitant, fill = log10P)) + 
+            geom_tile() #+ theme_bw() + scale_y_continuous(name = "log10(1/p)") + scale_x_continuous(name = "log2(semi-Bayes shrunk adjusted rate ratio)") + ggtitle(titleText)
+        #ggplotly(basePlot)
+        plot_ly(x = filteredData$Precipitant, y= filteredData$Base_Precipitant, 
+                z = filteredData$log10P, 
+                type = "heatmap", 
+                colorscale= "Earth",
+                showscale = F, ) %>% layout(autosize = F, width = 1000, height = 1000, title = titleText)
+    
+    
+}
+
+# Load the datafile with the risk estimates
 estimates <- read_csv("/Users/wfvdmei/Documents/Rotation Fall 2021/Shiny App/3DI Visualization/AdjustedEstimates/adjustedEstimatesProcessed.csv")
 
-# Define UI for application that draws a histogram
+# Create the user interface for the visualization
 ui <- fluidPage(
 
     # Application title
@@ -38,7 +72,7 @@ ui <- fluidPage(
         sidebarPanel(
             selectInput("plotType", "Type of Plot:",
                         list("Heat Map" = "heatMap", 
-                             "Volcano Plot" = "volcano")),
+                             "Volcano Plot" = "volcano"), selected = "volcano"),
             selectInput("outcome", "Outcome:",
                         list("Injury" = "injury", 
                              "Motor Vehicle Collision" = "mvc", 
@@ -47,20 +81,14 @@ ui <- fluidPage(
                         list("7-Fold" = "fold7", 
                              "25-Fold" = "fold25" 
                              )),
-            # allow creation of new items in the drop-down list
             selectizeInput(
                 'objectDrug', label = "Object Drug", choices = NULL,
                 options = list(maxOptions = 5, placeholder = "Select object drug")
             ),
-            selectizeInput(
-                'basePrecipitant', label = "Base Precipitant", choices = NULL,
-                options = list(maxOptions = 5)
-            ),
+            uiOutput("basePrecipitant"), 
+            actionButton(inputId = "createPlot", label = "Create Plot", icon = NULL, width = NULL),
         ),
         
-        
-
-        # Show a plot of the generated distribution
         mainPanel(
            plotlyOutput("plotInteractive")
         )
@@ -71,14 +99,35 @@ ui <- fluidPage(
 server <- function(input, output) {
     updateSelectizeInput(inputId = 'objectDrug', choices = estimates$Object, server = TRUE)
     updateSelectizeInput(inputId = 'basePrecipitant', choices = estimates$Base_Precipitant, server = TRUE)
-    #observeEvent(input$outcome, {
-     #   updateSelectInput(inputId = "estimateRange", selected = ifelse(input$outcome != "injury", "fold25", NULL))
-    #})
-    #observeEvent(input$outcome, {
-        #updateSelectizeInput(inputId = "objectDrug", options = ifelse(input$outcome != "injury",list(create = TRUE, maxOptions = 5), list(create = TRUE, maxOptions = 5)))
-    #})
-    output$plotInteractive <- renderPlotly(createPlot(outcome = input$outcome, plotType = input$plotType, objectDrug = input$objectDrug, basePrecipitant = input$basePrecipitant, estimateRange = input$estimateRange))
+    output$basePrecipitant <- renderUI({
+        if(input$plotType == "volcano"){
+            selectizeInput('basePrecipitant', label = "Base Precipitant", choices = NULL,
+                           options = list(maxOptions = 5))
+        }
+    })
+    observeEvent(input$plotType, {
+        updateSelectizeInput(inputId = 'basePrecipitant', choices = estimates$Base_Precipitant, server = TRUE)
+    })
+    observeEvent(input$outcome, {
+        updateSelectizeInput(inputId = 'basePrecipitant', choices = estimates$Base_Precipitant[estimates$Outcome == input$outcome], server = TRUE)
+    })
+    observeEvent(input$outcome, {
+        updateSelectizeInput(inputId = 'objectDrug', choices = estimates$Object[estimates$Outcome == input$outcome], server = TRUE)
+    })
+    observeEvent(input$estimateRange, {
+        updateSelectizeInput(inputId = 'basePrecipitant', choices = estimates$Base_Precipitant[estimates$range == input$estimateRange], server = TRUE)
+    })
+    observeEvent(input$estimateRange, {
+        updateSelectizeInput(inputId = 'objectDrug', choices = estimates$Object[estimates$range == input$estimateRange], server = TRUE, selected = NULL)
+    })
+    output$plotInteractive <- renderPlotly({ 
+            input$createPlot
+            if(input$plotType == "volcano"){
+                isolate(createVolcanoPlot(outcome = input$outcome,  objectDrug = input$objectDrug, basePrecipitant = input$basePrecipitant, estimateRange = input$estimateRange))
+            }
+            else {
+                isolate(createHeatMapPlot(outcome = input$outcome, objectDrug = input$objectDrug, estimateRange = input$estimateRange))}
+            })
 }
-
 # Run the application 
 shinyApp(ui = ui, server = server)
